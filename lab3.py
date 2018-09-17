adab4b50902ce96637ac0b775c61006ae7289993
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-09-17 12:31:48 -0500

    uniform cost search structure

--- lab3.py
+++ lab3.py
@@ -39,13 +39,34 @@ def main():
     (A, C); X; X
     '''
     input = fileinput.input()
-    in_line_1 = int(input[0])
-    in_line_2 = format(input[1])
-    in_line_3 = format(input[2])
+    max_h = int(input[0])
+    initial_state = format(input[1])
+    goal = format(input[2])
 
     #in_line_2 = '(B, A); (C, D, E); ()'
 
-    print("Line 1: %d\nLine 2: %s\nLine 3: %s" % (in_line_1, in_line_2, in_line_3))
+    print("Line 1: %d\nLine 2: %s\nLine 3: %s" % (max_h, initial_state, goal))
+
+
+    uniform_cost_search(max_h, goal, initial_state)
+
+
+def uniform_cost_search(max_h, goal, initial_state):
+    # Frontier list: list of dictionaries in format:
+    # node = {state: [[a, b], [c], []], cost: 4, parent: [[a, b, c], [], []], action: (1, 2)}
+    initial_node = {state: initial_state, cost: 0, parent: None, action: None}
+    frontier = [initial_node]
+    # list of states
+    visited = []
+    while true:
+        if len(frontier) == 0:
+            return false
+        node = frontier.pop()
+        if test_goal(node):
+            return node
+        visited.append(node.state)
+        frontier = expand(frontier, max_h, node.state)
+
 
 if __name__ == "__main__":
     main()
